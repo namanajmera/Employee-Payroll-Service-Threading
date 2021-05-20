24d1e268ec8a21156a4c68e312a07183e60ed0e0
Naman <cool.naman.ajmera@gmail.com>
2021-05-20 09:47:27 +0530

    Added: Create thread better response.

--- src/main/java/com/employeepayrollservice/EmployeePayrollDBService.java
+++ src/main/java/com/employeepayrollservice/EmployeePayrollDBService.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 public class EmployeePayrollDBService {
     private static EmployeePayrollDBService employeePayrollDBService;
+//    private static Logger log = Logger.getLogger(EmployeePayrollDBService.class.getName());
+    private int connectionCounter = 0;
 
     // creating the object of Signature and getting instance
     public static EmployeePayrollDBService getInstance() {
@@ -16,24 +18,27 @@ public class EmployeePayrollDBService {
         return employeePayrollDBService;
     }
 
-    public static Connection getConnection() {
+    public synchronized Connection getConnection() throws SQLException {
+        connectionCounter++;
         String jdbcURL = "jdbc:mysql://localhost:3307/payroll_service";
         String userName = "root";
         String password = "Spider@6426";
         Connection connection = null;
         // the DriverManager class will attempt to load available JDBC drivers
-        try {
-            connection = DriverManager.getConnection(jdbcURL, userName, password);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        System.out.println("Processing Thread : " + Thread.currentThread().getName() + "Connecting to database : " + jdbcURL);
+        connection = DriverManager.getConnection(jdbcURL, userName, password);
+        //which thread is actually going to execute my connection
+        System.out.println("Processing Thread : " + Thread.currentThread().getName() + " ID : " + connectionCounter
+                + " Connection is successful! " + connection);
         return connection;
     }
 
     public List<EmployeePayrollData> readData() {
         String sql = "SELECT * FROM employee_payroll;";
         List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
-        try (Connection connection = EmployeePayrollDBService.getConnection();) {
+        try (Connection connection = this.getConnection();) {
+            // A SQL statement is precompiled and stored in a PreparedStatement object.
+            //This object can then be used to efficiently execute this statement multiple times.
             PreparedStatement statement = connection.prepareStatement(sql);
             ResultSet resultSet = statement.executeQuery(sql);
             while (resultSet.next()) {
@@ -57,7 +62,7 @@ public class EmployeePayrollDBService {
         String sql = String.format(
                 "INSERT INTO employee_payroll (name,gender,salary,start) VALUES ('%s','%s','%s','%s')", name, gender,
                 salary, Date.valueOf(startDate));
-        try (Connection connection = EmployeePayrollDBService.getConnection();) {
+        try (Connection connection = this.getConnection();) {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             //generated keys should be made available for retrieval
             int rowAffected = preparedStatement.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
